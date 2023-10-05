8fe59feb2a4f01f3f59cca92b2c2d77907b1234c
Shubham.Pundir <shubham.pundir@timesinternet.in>
2023-10-05 13:08:30 +0530

    Changes for vertical Carausal usecase

--- app/src/main/java/com/jackandphantom/carousellayout/MainActivity.kt
+++ app/src/main/java/com/jackandphantom/carousellayout/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.jackandphantom.carousellayout
 
 import android.os.Bundle
+import android.view.Gravity
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import com.example.customviewimple.model.DataModel
 import com.jackandphantom.carousellayout.adapter.DataAdapter
 import com.jackandphantom.carousellayout.databinding.ActivityMainBinding
@@ -22,14 +24,25 @@ class MainActivity : AppCompatActivity() {
         list.add(DataModel(R.drawable.hobes, "Thi is cool"))
         list.add(DataModel(R.drawable.guypro, "Thi is cool"))
         list.add(DataModel(R.drawable.joker, "Thi is cool"))
+        list.add(DataModel(R.drawable.joker, "Thi is cool"))
+        list.add(DataModel(R.drawable.joker, "Thi is cool"))
+        list.add(DataModel(R.drawable.joker, "Thi is cool"))
        // list.add(DataModel(R.drawable.londonlove, "Thi is cool"))
 
         val adapter = DataAdapter(list)
 
         binding.recycler.apply {
             this.adapter = adapter
-            set3DItem(true)
+//            set3DItem(true)
+//            setInfinite(true)
             setAlpha(true)
+//            setIntervalRatio(0.7f)
+//            setFlat(true)
+            setIsScrollingEnabled(true)
+            setOrientation(RecyclerView.VERTICAL)
+//            set3DItem(true)
+//            setAlpha(true)
+
         }
 
         //Trigger the button and put your useCase to test different cases of adapter
